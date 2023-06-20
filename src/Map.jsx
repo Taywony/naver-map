dbf0a31e7c4a719c44484f6c0817ff51568b069e
taewon kim <taewon2659@gmail.com>
2023-06-20 18:25:53 +0900

    polygon 좌표 data get

--- src/Map.jsx
+++ src/Map.jsx
@@ -13,6 +13,7 @@ const Map = () => {
   const [sgisData, setSgisData] = useState([]);
   const [accessToken, setAccessToken] = useState(null);
   const [selectedRadio, setSelectedRadio] = useState(RADIO[0].value);
+  const [polygonData, setPolygonData] = useState();
 
   //accessToken 받아올때 넣는 body 값
   const getAccessToken = {
@@ -26,6 +27,7 @@ const Map = () => {
     // cd: "24",
   };
 
+  // naver map 표시
   useEffect(() => {
     const { naver } = window;
     if (!mapElement.current || !naver) return;
@@ -33,19 +35,48 @@ const Map = () => {
     // 지도에 표시할 위치의 위도와 경도 좌표를 파라미터로 넣어줍니다.
     const location = new naver.maps.LatLng(37.5682, 126.8973);
     const mapOptions = {
-      center: location,
-      zoom: 17,
+      center: new naver.maps.LatLng(35.5, 127.4), //초기값 전체지도 보이게
+      zoom: 7,
       zoomControl: true,
       zoomControlOptions: {
         position: naver.maps.Position.TOP_RIGHT,
       },
     };
     const map = new naver.maps.Map(mapElement.current, mapOptions);
-    new naver.maps.Marker({
-      position: location,
-      map,
+    // new naver.maps.Marker({
+    //   position: location,
+    //   map,
+    // });
+
+    let polygon = new naver.maps.Polygon({
+      map: map,
+      paths: [
+        [
+          new naver.maps.LatLng(37.37544345085402, 127.11224555969238),
+          new naver.maps.LatLng(37.37230584065902, 127.10791110992432),
+          new naver.maps.LatLng(37.35975408751081, 127.10795402526855),
+          new naver.maps.LatLng(37.359924641705476, 127.11576461791992),
+          new naver.maps.LatLng(37.35931064479073, 127.12211608886719),
+          new naver.maps.LatLng(37.36043630196386, 127.12293148040771),
+          new naver.maps.LatLng(37.36354029942161, 127.12310314178465),
+          new naver.maps.LatLng(37.365211629488016, 127.12456226348876),
+          new naver.maps.LatLng(37.37544345085402, 127.11224555969238),
+        ],
+      ],
+      fillColor: "#ff0000",
+      fillOpacity: 0.3,
+      strokeColor: "#ff0000",
+      strokeOpacity: 0.6,
+      strokeWeight: 3,
     });
   }, []);
+
+  //polygon 좌표 가져오는 api
+  useEffect(() => {
+    axios("/sido.json").then((res) => setPolygonData(res));
+  }, []);
+
+  console.log("polygonData", polygonData);
 
   //accessToken 받아오는 api
   useEffect(() => {
@@ -72,6 +103,7 @@ const Map = () => {
 
   console.log("sgisData", sgisData);
 
+  console.log("------------------------------------------");
   return (
     <div className="view">
       <div
